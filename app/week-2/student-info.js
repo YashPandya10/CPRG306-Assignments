77847a2b791598936f5146ceff03601e73fd1360
YashPandya10 <123474152+YashPandya10@users.noreply.github.com>
2025-09-19 20:24:38 -0600

    Initial commit - Next.js project

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <div>
-      <h2>Student Information</h2>
+      <h2>Yash Pandya</h2>
       <p>
         GitHub:{" "}
         <Link
-          href="https://github.com/yashpandya/cprg306-assignments"
+          href="https://github.com/YashPandya10/CPRG306-Assignments.git"
           target="_blank"
           rel="noopener noreferrer"
         >
